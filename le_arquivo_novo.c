5cf0b2ff15655912f5a36dde99eb4c96cf5dda75
CarlaNunes <carla.nunes@usp.br>
2021-10-17 15:07:45 -0300

    Versão com strsep

---
+++ le_arquivo_novo.c
@@ -0,0 +1,69 @@
+#define _GNU_SOURCE
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+#include "funcoes.h"
+
+typedef int tipoChave;
+typedef int rrn;
+typedef char* string;
+
+
+
+
+int le_arquivo_novo(FILE *arquivo){
+    int i = 0;
+    Metro metro[200];
+    
+    int bufSize = 300;
+    char cabecalho[bufSize];
+    char *token;
+
+    if ((arquivo = fopen("estacoes.csv", "r")) == NULL) {
+        printf("Erro ao abrir o arquivo.\n");
+    } else {
+#if 1 
+        const char separador[2] = ",";
+        char buffer[bufSize];
+
+       
+        fgets (cabecalho, bufSize, arquivo); 
+        while( fgets (buffer, bufSize, arquivo)!=NULL ) {
+          
+              token = strsep(&string,separador);
+              metro[i].codEstacao = atoi(token);  
+
+            i += 1;
+             
+        }
+        //Testando
+        
+        for(i = 0; i < 200; i++){
+            printf("----------------------\n");
+            printf("codEstação: %d\n", metro[i].codEstacao);
+            printf("nomeEstação: %s\n", metro[i].nomeEstacao);
+            printf("codLinha: %d \n",metro[i].codLinha);
+            printf("NomeLinha: %s\n", metro[i].nomeLinha);
+            printf("codProxEst: %d \n",metro[i].codProxEst);
+            printf("distanciaProxEst: %d \n",metro[i].distanciaProxEst);
+            printf("codLinhainteg: %d \n",metro[i].codEstacaoInteg);
+            printf("codEstacaoInteg: %d \n",metro[i].codEstacaoInteg);
+            
+            
+            
+            printf("\n");
+        }
+        
+#endif
+        fclose(arquivo);
+        //free(metro[i].nomeEstacao);
+        //free(metro[i].nomeLinha);
+        //free(r);
+    }
+    
+    return i;
+}
+
