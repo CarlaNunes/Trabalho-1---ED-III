199847e9aae056f901b5e680738d898a56081013
CarlaNunes <carla.nunes@usp.br>
2021-10-23 22:24:47 -0300

    Problemas de falha de segmentação
    Ainda não consegui achar o erro.

--- escreve_Binario2.c
+++ escreve_Binario2.c
@@ -8,11 +8,12 @@
 //Colocar o '|' 
 
 //Versão com impressão campo a campo
-//Também com preenchimento de campos "pequenos com lixo"
+//Também com preenchimento de campos "pequenos com lixo"-> Inserir 
 void escreve_Binario2(FILE *arquivo, int cont,char nomeArquivoBinario[20]){
+  printf("CHEGUEI AQUI 2!!!!");
   Metro metro[200];
 
-  char status = '0',removido;//Primeiro item do cabeçalho
+  char status = '0',removido = '1';//Primeiro item do cabeçalho
   char grau[3],data1[10],data2[10];//Variaveis auxiliares para armazenar os valores da struct para impressão.
   int quantidadeEstacoes = cont;
   char lixo[] = "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$";//Terceiro item do cabeçalho
@@ -26,29 +27,31 @@ void escreve_Binario2(FILE *arquivo, int cont,char nomeArquivoBinario[20]){
         printf("\n Falha no carregamento do arquivo.\n");
     }
     
-   //------Início escrita em arquivo do cabeçalho--------------
-     fwrite("removido",sizeof(char),1,arquivo);
-     fwrite("Tamanho Registro",sizeof(char) ,1, arquivo);
-     fwrite("cod Estacao",sizeof(char) ,1, arquivo);
-     fwrite("cod Linha",sizeof(char) ,1, arquivo);
-     fwrite("codProx Estacao",sizeof(char) ,1, arquivo);
-     fwrite("distProx Estacao",sizeof(char) ,1, arquivo);
-     fwrite("codEst Integra",sizeof(char) ,1, arquivo);
-     fwrite("nome Estacao",sizeof(char) ,1, arquivo);
-     fwrite("nome Linha",sizeof(char) ,1, arquivo);
-           
+  else{
 
+     //------Início escrita em arquivo do cabeçalho--------------
+      printf("CHEGUEI AQUI 1!!!!");
 
-     //fwrite(&lixo, sizeof(char) ,1, arquivo);
-  //------Fim escrita em arquivo do cabeçalho--------------
-  //else{
-       int i;
+      fwrite("removido",sizeof(char),1,arquivo);
+      fwrite("Tamanho Registro",sizeof(char) ,1, arquivo);
+      fwrite("cod Estacao",sizeof(char) ,1, arquivo);
+      fwrite("cod Linha",sizeof(char) ,1, arquivo);
+      fwrite("codProx Estacao",sizeof(char) ,1, arquivo);
+      fwrite("distProx Estacao",sizeof(char) ,1, arquivo);
+      fwrite("codEst Integra",sizeof(char) ,1, arquivo);
+      fwrite("nome Estacao",sizeof(char) ,1, arquivo);
+      fwrite("nome Linha",sizeof(char) ,1, arquivo);
+       //fwrite(&lixo, sizeof(char) ,1, arquivo);
+  //------Fim escrita em arquivo do cabeçalho-------------- 
+
+      int i;
         for(i = 0; i < quantidadeEstacoes ; i++){
       //------------Início escrita dos valores da struct--------
-            removido = '1'; //Ver um mdodo de mudar depois.
+           // removido = '1'; //Ver um mdodo de mudar depois.
             //tamReg = strlen(metro); Dando erro aqui, depois resolver
 
             fwrite(&removido,1,sizeof(char),arquivo);
+
 
             int codE = metro[i].codEstacao;
             fwrite(&codE,1,sizeof(int),arquivo);
@@ -80,7 +83,7 @@ void escreve_Binario2(FILE *arquivo, int cont,char nomeArquivoBinario[20]){
 
               
             //------------Fim escrita dos valores da struct--------
-       // }
+        }
   }
       
            
